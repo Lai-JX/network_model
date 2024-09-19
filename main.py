e33a781a47bd3510531765d4e178a6c735e1dc18
D Sun <1075079562@qq.com>
2024-09-19 16:59:30 +0800

    add some tests

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import random
 random.seed(2024)
 
-def build_graph(data_path, num_sample=1000):
+def build_graph(data_path, num_sample=500):
     G = nx.Graph()
 
     # 读取txt文件
@@ -14,13 +14,21 @@ def build_graph(data_path, num_sample=1000):
     # 创建节点列表并找出唯一的节点ID
     nodes = set()
     for line in edges:
+    # for line in lines[1:]:
         from_node, to_node = line.strip().split(",")
         nodes.add(from_node)
         nodes.add(to_node)
         G.add_edge(from_node, to_node)
     return G, nodes
 
+
+
 g, nodes = build_graph('./data/git_web_ml/musae_git_edges.csv')
 print(g)
+
 nx.draw(G=g,node_size=10)
 plt.show()
+
+
+
+

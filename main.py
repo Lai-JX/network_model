94f96906b23014027a408b7c4f8aba70afa7adfb
Lai-JX <2840026059@qq.com>
2024-09-21 00:19:49 +0800

    add some util function in main.py

--- main.py
+++ main.py
@@ -24,13 +24,133 @@ def build_graph(data_path, num_sample=500):
         G.add_edge(from_node, to_node)
     return G, nodes
 
+def draw_graph(G, node_size=None):
+    '''
+        Draw graph G
+        The node size is determined based on the degree if arg node_size is None.
+    '''
+    #### draw graph ####
+    fig, ax = plt.subplots(figsize=(20, 15))
+    pos = nx.spring_layout(G, k=0.15, seed=4572321)
+    # node_color = [community_index[n] for n in H]
+    if not isinstance(node_size, list):
+        node_size = [d * 3 for n, d in G.degree()]
+    nx.draw_networkx(
+        G,
+        pos=pos,
+        with_labels=False,
+        # node_color=node_color,
+        node_size=node_size,
+        edge_color="gainsboro",
+        width=1,
+        alpha=0.6,
+    )
+
+    # Title/legend
+    font = {"color": "k", "fontweight": "bold", "fontsize": 20}
+    # ax.set_title("Gene functional association network (C. elegans)", font)
+    # Change font color for legend
+    font["color"] = "r"
+
+    # ax.text(
+    #     0.80,
+    #     0.10,
+    #     "node color = community structure",
+    #     horizontalalignment="center",
+    #     transform=ax.transAxes,
+    #     fontdict=font,
+    # )
+    # ax.text(
+    #     0.80,
+    #     0.06,
+    #     "node size = betweenness centrality",
+    #     horizontalalignment="center",
+    #     transform=ax.transAxes,
+    #     fontdict=font,
+    # )
+
+    # Resize figure for label readability
+    ax.margins(0.1, 0.05)
+    fig.tight_layout()
+    plt.axis("off")
+    plt.show()
+    
 
 
-# g, nodes = build_graph('./data/git_web_ml/musae_git_edges.csv')
-# print(g)
-#
-# nx.draw(G=g,node_size=10)
-# plt.show()
+def find_subgraph(G, delete_degree_min=0):
+    '''
+        Remove nodes (degree < delete_degree_min)
+        Find largest_component 
+        Returns the corresponding subgraph
+    '''
+    # remove low-degree nodes
+    low_degree = [n for n, d in G.degree() if d < delete_degree_min]
+    G.remove_nodes_from(low_degree)
+    print("number_connected_components:",nx.number_connected_components(G))
+
+    # components = nx.connected_components(G)
+    # component_graphs=[G.subgraph(c).copy() for c in components]
+
+    # largest connected component
+    largest_component = max(nx.connected_components(G), key=len)
+    H = G.subgraph(largest_component)
+    return H
+
+def draw_betweenness_centrality(G):
+    '''
+        from:https://networkx.org/documentation/stable/auto_examples/algorithms/plot_betweenness_centrality.html
+    '''
+
+    # compute centrality
+    centrality = nx.betweenness_centrality(G, k=10, endpoints=True) # 
+
+    # compute community structure
+    lpc = nx.community.label_propagation_communities(G)
+    community_index = {n: i for i, com in enumerate(lpc) for n in com}
+
+    #### draw graph ####
+    fig, ax = plt.subplots(figsize=(20, 15))
+    pos = nx.spring_layout(G, k=0.15, seed=4572321)
+    node_color = [community_index[n] for n in G]
+    node_size = [v * 20000 for v in centrality.values()]
+    nx.draw_networkx(
+        G,
+        pos=pos,
+        with_labels=False,
+        node_color=node_color,
+        node_size=node_size,
+        edge_color="gainsboro",
+        alpha=0.4,
+    )
+
+    # Title/legend
+    font = {"color": "k", "fontweight": "bold", "fontsize": 20}
+    ax.set_title("Gene functional association network (C. elegans)", font)
+    # Change font color for legend
+    font["color"] = "r"
+
+    ax.text(
+        0.80,
+        0.10,
+        "node color = community structure",
+        horizontalalignment="center",
+        transform=ax.transAxes,
+        fontdict=font,
+    )
+    ax.text(
+        0.80,
+        0.06,
+        "node size = betweenness centrality",
+        horizontalalignment="center",
+        transform=ax.transAxes,
+        fontdict=font,
+    )
+
+    # Resize figure for label readability
+    ax.margins(0.1, 0.05)
+    fig.tight_layout()
+    plt.axis("off")
+    plt.show()
 
 
 
--- test-ljx.py
+++ test-ljx.py
@@ -1,79 +1,17 @@
 from random import sample
 import networkx as nx
 import matplotlib.pyplot as plt
-from main import build_graph
+from main import build_graph, draw_graph, find_subgraph,draw_betweenness_centrality
+import numpy as np
+
 # build graph
-G, nodes = build_graph('./data/git_web_ml/musae_git_edges.csv', None)
+G, nodes = build_graph('./data/git_web_ml/musae_git_edges.csv', 1000)
 print(G)
 # Gcc = g.subgraph(sorted(nx.connected_components(g), key=len, reverse=True)[0])
 
+draw_graph(G)
 
-# remove randomly selected nodes (to make example fast)
-# num_to_remove = int(len(G) / 1.5)
-# nodes = sample(list(G.nodes), num_to_remove)
-# G.remove_nodes_from(nodes)
-
-# remove low-degree nodes
-low_degree = [n for n, d in G.degree() if d < 0]
-G.remove_nodes_from(low_degree)
-print(G)
-print("number_connected_components:",nx.number_connected_components(G))
-
-# largest connected component
-components = nx.connected_components(G)
-component_graphs=[G.subgraph(c).copy() for c in components]
-
-largest_component = max(nx.connected_components(G), key=len)
-
-H = G.subgraph(largest_component)
+H = find_subgraph(G)
 print(H)
-# compute centrality
-centrality = nx.betweenness_centrality(H, k=10, endpoints=True) # 
 
-# compute community structure
-lpc = nx.community.label_propagation_communities(H)
-community_index = {n: i for i, com in enumerate(lpc) for n in com}
-
-#### draw graph ####
-fig, ax = plt.subplots(figsize=(20, 15))
-pos = nx.spring_layout(H, k=0.15, seed=4572321)
-node_color = [community_index[n] for n in H]
-node_size = [v * 20000 for v in centrality.values()]
-nx.draw_networkx(
-    H,
-    pos=pos,
-    with_labels=False,
-    node_color=node_color,
-    node_size=node_size,
-    edge_color="gainsboro",
-    alpha=0.4,
-)
-
-# Title/legend
-font = {"color": "k", "fontweight": "bold", "fontsize": 20}
-ax.set_title("Gene functional association network (C. elegans)", font)
-# Change font color for legend
-font["color"] = "r"
-
-ax.text(
-    0.80,
-    0.10,
-    "node color = community structure",
-    horizontalalignment="center",
-    transform=ax.transAxes,
-    fontdict=font,
-)
-ax.text(
-    0.80,
-    0.06,
-    "node size = betweenness centrality",
-    horizontalalignment="center",
-    transform=ax.transAxes,
-    fontdict=font,
-)
-
-# Resize figure for label readability
-ax.margins(0.1, 0.05)
-fig.tight_layout()
-plt.axis("off")
-plt.show()
+draw_betweenness_centrality(H)

70d55e2080433c2eb70f429950fe976de088b39f
D Sun <1075079562@qq.com>
2024-09-21 17:00:18 +0800

    add betweenness and intentional attack

--- apitest.py
+++ apitest.py
@@ -1,6 +1,5 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
 
 from main import build_graph
 
@@ -19,16 +18,13 @@ ax.set_axis_off()
 fig.tight_layout()
 plt.show()
 
-
 # diameter
 print('diameter:', nx.diameter(g))
-
 
 # clustering coefficient
 from cluster_coefficient import average_cluster_coefficient
 
 print('average clustering coefficient:', average_cluster_coefficient(g))
-
 
 # average path length (of each connected component)
 component_count = 1
@@ -36,14 +32,12 @@ for component in (g.subgraph(component).copy() for component in nx.connected_com
     print('average path length of component', component_count, ':', nx.average_shortest_path_length(component))
     component_count += 1
 
-
 # degree
 from degree import draw_degree_distribution, average_degree, draw_degree_rank
 
 print('average degree:', average_degree(g))
 draw_degree_distribution(g)
 draw_degree_rank(g)
-
 
 # coreness
 from coreness import draw_coreness_distribution, draw_k_core
@@ -53,8 +47,18 @@ draw_k_core(g, 3)
 draw_k_core(g, 2)
 draw_k_core(g, 1)
 
-
 # closeness
 from closeness import draw_closeness_distribution
 
 draw_closeness_distribution(g)
+
+# edge betweenness
+from betweenness import draw_edge_betweenness_distribution, get_max_betweenness_edge, \
+    intentional_attack_edge_betweenness, random_attack_edge_betweenness
+
+draw_edge_betweenness_distribution(g)
+max_betweenness, max_edge = get_max_betweenness_edge(g)
+print('max betweenness:', max_betweenness)
+print('max edge:', max_edge)
+print('intentionally attacked graph', intentional_attack_edge_betweenness(g))
+random_attack_edge_betweenness(g)
---
+++ dsun-tk-test.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.title("Modeling of Complex Networks")
+root.geometry("1200x800+200+100")
+cv = Canvas(root, background='blue', width=1200, height=600)
+cv.pack()
+
+root.mainloop()
+
